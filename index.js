f22dbf27b8d3de654838ec07526b0d9b94ae5049
muyingo-abubakar <muyingoabubakar73@gmail.com>
2024-01-06 22:17:50 +0300

    displaying texts with js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-console.log("Hello world");
-console.log("I like pizza");
+// console.log("Hello world");
+// console.log("I like pizza");
 
-window.alert(`This ia alert!`);
-window.alert(`I like pizza`);
+// window.alert(`This ia alert!`);
+// window.alert(`I like pizza`);
 
+document.getElementById("myH1").textContent = `ABUBAKAR`;
+document.getElementById("myP").textContent = `MUYINGO`;
+document.getElementById(`myP2`).innerHTML = `BAKER`;
 //This is a comment
+
+/* All the next 
+is a commet unless closed */
