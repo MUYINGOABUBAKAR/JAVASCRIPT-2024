31ed995962a13d8418bbb9a4a2485f2cde19a220
muyingo-abubakar <muyingoabubakar73@gmail.com>
2024-01-07 14:52:26 +0300

    variables strings

--- index.js
+++ index.js
@@ -16,10 +16,21 @@ let y = 100;
 console.log(y);
 */
 
-let age = 25;
-let price = 300;
-let gpa = 1.02;
+//NUMBERS
+// let age = 25;
+// let price = 300;
+// let gpa = 1.02;
 
-console.log(typeof age);
-console.log(`Muyingo Abubakar age is ${age}
- and he likes a pizza of $${price} at ${gpa}`);
+// console.log(typeof age);
+// console.log(`Muyingo Abubakar age is ${age}
+//  and he likes a pizza of $${price} at ${gpa}`);
+
+//STRING
+
+let firstName = "Muyingo";
+let secondName = "Abubakar";
+let career = "programming";
+let favoriteFood = "pasta";
+
+console.log(`He is ${firstName} ${secondName} who loves a 
+${career} career and he loves to eat ${favoriteFood} much`);
